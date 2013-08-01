32985110921776c1bf340954a76ab475c132b4ad
Chris Peterson <cpeterson@mozilla.com>
2013-08-01 14:28:03 -0700

    LocationBlocklist.java: fix indentation

--- src/org/mozilla/mozstumbler/LocationBlockList.java
+++ src/org/mozilla/mozstumbler/LocationBlockList.java
@@ -4,13 +4,12 @@ import android.location.Location;
 import android.util.Log;
 
 final class LocationBlockList {
-    private static final String LOGTAG         = LocationBlockList.class.getName();
-    private static final double MAX_ALTITUDE   = 8848;                             // Mount Everest's altitude in
-                                                                                    // meters
-    private static final double MIN_ALTITUDE   = -418;                             // Dead Sea's altitude in meters
-    private static final float  MAX_SPEED      = 340.29f;                          // Mach 1 in meters/second
-    private static final float  MAX_INACCURACY = 500;                              // meter radius
-    private static final long   MIN_TIMESTAMP  = 946684801;                        // 2000-01-01 00:00:01
+    private static final String LOGTAG          = LocationBlockList.class.getName();
+    private static final double MAX_ALTITUDE    = 8848;      // Mount Everest's altitude in meters
+    private static final double MIN_ALTITUDE    = -418;      // Dead Sea's altitude in meters
+    private static final float  MAX_SPEED       = 340.29f;   // Mach 1 in meters/second
+    private static final float  MAX_INACCURACY  = 500;       // meter radius
+    private static final long   MIN_TIMESTAMP   = 946684801; // 2000-01-01 00:00:01
 
     private LocationBlockList() {
     }

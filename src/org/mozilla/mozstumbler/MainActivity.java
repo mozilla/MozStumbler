6d2d57f698b9c437efd81c1b818f7a8f9fc4f5c3
Doug Turner <doug.turner@gmail.com>
2013-08-04 18:54:49 -0700

    No need for a settings menu until we have settings.

--- src/org/mozilla/mozstumbler/MainActivity.java
+++ src/org/mozilla/mozstumbler/MainActivity.java
@@ -220,9 +220,7 @@ public final class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        return false;
     }
 
     @TargetApi(9)

d871a516504cf2b48432ec630a945e128a4cb441
Chris Peterson <cpeterson@mozilla.com>
2013-09-14 11:06:41 -0700

    Resurrect SSIDBlockList.java

--- src/org/mozilla/mozstumbler/SSIDBlockList.java
+++ src/org/mozilla/mozstumbler/SSIDBlockList.java
@@ -3,11 +3,86 @@ package org.mozilla.mozstumbler;
 import android.net.wifi.ScanResult;
 
 final class SSIDBlockList {
+    private static final String[] PREFIX_LIST = {
+        // Mobile devices
+        "AndroidAP",
+        "AndroidHotspot",
+        "Android Hotspot",
+        "barnacle", // Android tether app
+        "Galaxy Note",
+        "Galaxy S",
+        "Galaxy Tab",
+        "HTC ",
+        "iPhone",
+        "LG-MS770",
+        "LG-MS870",
+        "LG VS910 4G",
+        "LG Vortex",
+        "MIFI",
+        "MiFi",
+        "myLGNet",
+        "myTouch 4G Hotspot",
+        "PhoneAP",
+        "SCH-I",
+        "Sprint MiFi",
+        "Verizon ",
+        "Verizon-",
+        "VirginMobile MiFi",
+
+        // Transportation Wi-Fi
+        "ac_transit_wifi_bus",
+        "AmtrakConnect",
+        "Amtrak_",
+        "amtrak_",
+        "GBUS",
+        "GBusWifi",
+        "SF Shuttle Wireless",
+        "ShuttleWiFi",
+        "SST-PR-1", // Sears Home Service van hotspot?!
+        "wifi_rail", // BART
+    };
+
+    private static final String[] SUFFIX_LIST = {
+        // Mobile devices
+        "iPhone",
+        "iphone",
+        "MIFI",
+        "MIFI",
+        "MiFi",
+        "Mifi",
+        "mifi",
+        "mi-fi",
+        "MyWi",
+        "Phone",
+        "Portable Hotspot",
+        "Tether",
+        "tether",
+
+        // Google's SSID opt-out
+        "_nomap",
+    };
+
     private SSIDBlockList() {
     }
 
     static boolean contains(ScanResult scanResult) {
         String SSID = scanResult.SSID;
-        return SSID == null || "".equals(SSID) || SSID.endsWith("_nomap");
+        if (SSID == null) {
+            return true; // no SSID?
+        }
+
+        for (String prefix : PREFIX_LIST) {
+            if (SSID.startsWith(prefix)) {
+                return true; // blocked!
+            }
+        }
+
+        for (String suffix : SUFFIX_LIST) {
+            if (SSID.endsWith(suffix)) {
+                return true; // blocked!
+            }
+        }
+
+        return false; // OK
     }
 }

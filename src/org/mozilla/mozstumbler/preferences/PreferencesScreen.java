17a27b979ec5fd5754f475af209b2a39fbd2f490
Denevell <aaron@denevell.org>
2013-10-31 10:20:30 +0000

    Fixed problem #164 on older phones.
    We're now using the deprecated preferences activity.

--- src/org/mozilla/mozstumbler/preferences/PreferencesScreen.java
+++ src/org/mozilla/mozstumbler/preferences/PreferencesScreen.java
@@ -7,57 +7,47 @@ import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 
 public class PreferencesScreen extends PreferenceActivity {
 
+    private EditTextPreference mNicknamePreference;
+    private Prefs mPrefs;
+
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();        
+        addPreferencesFromResource(R.xml.preferences);
+        mNicknamePreference = (EditTextPreference) getPreferenceManager().findPreference("nickname");
+        mPrefs = new Prefs(this);
+
+        String nickname = mPrefs.getNickname();
+        setNicknamePreferenceTitle(nickname);
+        setNicknameListener();
+    }
+
+    public void setNicknameListener() {
+        mNicknamePreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                if ((newValue instanceof String) == false) {
+                    return false;
+                }
+                String input = (String) newValue;
+                String nickname = input.trim();
+                mPrefs.setNickname(nickname);
+                setNicknamePreferenceTitle(nickname);
+                return true;
+            }
+        });
+    }
+
+    public void setNicknamePreferenceTitle(String nickname) {
+        if (nickname != null && nickname.length()>0) {
+            mNicknamePreference.setTitle(getString(R.string.enter_nickname_title) + " " + nickname);
+        } else {
+           mNicknamePreference.setTitle(getString(R.string.enter_nickname));
+       }
     }
     
-    public static class MyPreferenceFragment extends PreferenceFragment
-    {
-        private EditTextPreference mNicknamePreference;
-        private Prefs mPrefs;
-
-        @Override
-        public void onCreate(final Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-            mNicknamePreference = (EditTextPreference) getPreferenceManager().findPreference("nickname");
-            mPrefs = new Prefs(getActivity().getApplicationContext());
-
-            String nickname = mPrefs.getNickname();
-            setNicknamePreferenceTitle(nickname);
-            setNicknameListener();
-        }
-
-        public void setNicknameListener() {
-            mNicknamePreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    if ((newValue instanceof String) == false) {
-                        return false;
-                    }
-                    String input = (String) newValue;
-                    String nickname = input.trim();
-                    mPrefs.setNickname(nickname);
-                    setNicknamePreferenceTitle(nickname);
-                    return true;
-                }
-            });
-        }
-
-        public void setNicknamePreferenceTitle(String nickname) {
-            if (nickname != null && nickname.length()>0) {
-                mNicknamePreference.setTitle(getString(R.string.enter_nickname_title) + " " + nickname);
-            } else {
-                mNicknamePreference.setTitle(getString(R.string.enter_nickname));
-            }
-        }
-    }    
-
 }

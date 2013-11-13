899ec9214b356e4f83a83b115cf5dd35ad85bde6
Tomer Cohen <tomer@users.noreply.github.com>
2013-11-14 00:36:40 +0200

    Removed rail stations in Israel because stations are not mobile enough.
    https://github.com/dougt/MozStumbler/pull/218#commitcomment-4590374

--- src/org/mozilla/mozstumbler/SSIDBlockList.java
+++ src/org/mozilla/mozstumbler/SSIDBlockList.java
@@ -43,7 +43,6 @@ final class SSIDBlockList {
         "egged.co.il", // Egged transportation services (Israel)
         "gb-tours.com", // GB Tours transportation services (Israel)
         "ISRAEL-RAILWAYS",
-        //"S-ISRAEL-RAILWAYS",  // Israel Railways stations
         "Omni-WiFi", // Omnibus transportation services (Israel)
     };
 

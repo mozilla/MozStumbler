0419b9a700b5ec3da07b07c2ac76b23019bde027
Chris Peterson <cpeterson@mozilla.com>
2014-01-12 22:59:51 -0800

    Always close HTTP response’s BufferedReader

--- src/org/mozilla/mozstumbler/Reporter.java
+++ src/org/mozilla/mozstumbler/Reporter.java
@@ -226,16 +226,24 @@ final class Reporter extends BroadcastReceiver {
                         }
                         Log.e(LOGTAG, "urlConnection returned " + code);
 
-                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                        BufferedReader r = new BufferedReader(new InputStreamReader(in));
-                        StringBuilder total = new StringBuilder(in.available());
-                        String line;
-                        while ((line = r.readLine()) != null) {
-                            total.append(line);
+                        BufferedReader r = null;
+                        try {
+                            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                            r = new BufferedReader(new InputStreamReader(in));
+                            StringBuilder total = new StringBuilder(in.available());
+                            String line;
+                            while ((line = r.readLine()) != null) {
+                                total.append(line);
+                            }
+                            Log.d(LOGTAG, "response was: \n" + total + "\n");
+                        } catch (Exception e) {
+                            Log.e(LOGTAG, "", e);
+                        } finally {
+                            if (r != null) {
+                                r.close();
+                                r = null;
+                            }
                         }
-                        r.close();
-
-                        Log.d(LOGTAG, "response was: \n" + total + "\n");
                     } catch (JSONException jsonex) {
                         Log.e(LOGTAG, "error wrapping data as a batch", jsonex);
                     } catch (Exception ex) {

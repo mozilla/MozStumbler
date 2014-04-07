ba52903240504d958e07ac80c1562d39a432c906
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-04-07 18:32:14 -0300

    Remove mLooper from the service and the handler from the Reporter.
    It looks like the only reason why it was being used was so that
    date synchronization could happen in a separate thread. Ever since
    we started using a content provider + sync adapter, this is no longer
    a concern.

--- src/org/mozilla/mozstumbler/ScannerService.java
+++ src/org/mozilla/mozstumbler/ScannerService.java
@@ -11,8 +11,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.RemoteException;
@@ -30,7 +28,6 @@ public final class ScannerService extends Service {
     private static final String INTENT_TURN_OFF = MESSAGE_TOPIC + ".TURN_ME_OFF";
     private Scanner             mScanner;
     private Reporter            mReporter;
-    private LooperThread        mLooper;
     private PendingIntent       mWakeIntent;
     private BroadcastReceiver   mTurnOffReceiver;
     private boolean             mIsBound;
@@ -47,50 +44,35 @@ public final class ScannerService extends Service {
                 return;
             }
 
-            mLooper.post(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        CharSequence title = getResources().getText(R.string.service_name);
-                        CharSequence text = getResources().getText(R.string.service_scanning);
-                        postNotification(title, text);
-                        mScanner.startScanning();
+            CharSequence title = getResources().getText(R.string.service_name);
+            CharSequence text = getResources().getText(R.string.service_scanning);
+            postNotification(title, text);
+            mScanner.startScanning();
 
-                        // keep us awake.
-                        Context cxt = getApplicationContext();
-                        Calendar cal = Calendar.getInstance();
-                        Intent intent = new Intent(cxt, ScannerService.class);
-                        mWakeIntent = PendingIntent.getService(cxt, 0, intent, 0);
-                        AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), WAKE_TIMEOUT, mWakeIntent);
-
-                    } catch (Exception e) {
-                        Log.d(LOGTAG, "looper shat itself : " + e);
-                    }
-                }
-            });
+            // keep us awake.
+            Context cxt = getApplicationContext();
+            Calendar cal = Calendar.getInstance();
+            Intent intent = new Intent(cxt, ScannerService.class);
+            mWakeIntent = PendingIntent.getService(cxt, 0, intent, 0);
+            AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), WAKE_TIMEOUT, mWakeIntent);
         }
 
         @Override
         public void stopScanning() throws RemoteException {
             if (mScanner.isScanning()) {
-                mLooper.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                        alarm.cancel(mWakeIntent);
+                AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                alarm.cancel(mWakeIntent);
 
-                        cancelNotification();
-                        stopForeground(true);
+                cancelNotification();
+                stopForeground(true);
 
-                        mScanner.stopScanning();
-                        mReporter.flush();
-                        SyncUtils.TriggerRefresh(false);
-                        if (!mIsBound) {
-                            stopSelf();
-                        }
-                    }
-                });
+                mScanner.stopScanning();
+                mReporter.flush();
+                SyncUtils.TriggerRefresh(false);
+                if (!mIsBound) {
+                    stopSelf();
+                }
             }
         }
 
@@ -146,22 +128,6 @@ public final class ScannerService extends Service {
 
     };
 
-    private final class LooperThread extends HandlerThread {
-        private Handler mHandler;
-
-        public LooperThread() {
-            super("ScannerService");
-        }
-
-        public synchronized void waitUntilReady() {
-            mHandler = new Handler(getLooper());
-        }
-
-        void post(Runnable runnable) {
-            mHandler.post(runnable);
-        }
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -186,10 +152,7 @@ public final class ScannerService extends Service {
         registerReceiver(mTurnOffReceiver, intentFilter);
 
         mScanner = new Scanner(this);
-        mLooper = new LooperThread();
-        mLooper.start();
-        mLooper.waitUntilReady();
-        mReporter = new Reporter(this, mLooper.getLooper());
+        mReporter = new Reporter(this);
     }
 
     @Override
@@ -202,42 +165,20 @@ public final class ScannerService extends Service {
 
         cancelNotification();
 
-        // Quit the handler thread's looper safely.
-        try {
-            mLooper.post(new Runnable() {
-                @Override
-                public void run() {
-                    mReporter.shutdown();
-                    mLooper.quit();
-                }
-            });
-            Looper looper = mLooper.getLooper();
-            if (looper != null) {
-                looper.getThread().join(5000);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        mReporter.shutdown();
         mReporter = null;
-        mLooper = null;
         mScanner = null;
     }
 
     private void postNotification(final CharSequence title, final CharSequence text) {
-        mLooper.post(new Runnable() {
-            @Override
-            public void run() {
-                Context ctx = getApplicationContext();
-                Intent notificationIntent = new Intent(ctx, MainActivity.class);
-                notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_FROM_BACKGROUND);
-                PendingIntent contentIntent = PendingIntent.getActivity(ctx, NOTIFICATION_ID, notificationIntent,
-                        PendingIntent.FLAG_CANCEL_CURRENT);
-                int icon = R.drawable.ic_status_scanning;
-                Notification n = buildNotification(ctx, icon, title, text, contentIntent);
-                startForeground(NOTIFICATION_ID, n);
-            }
-        });
+        Context ctx = getApplicationContext();
+        Intent notificationIntent = new Intent(ctx, MainActivity.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_FROM_BACKGROUND);
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, NOTIFICATION_ID, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+        int icon = R.drawable.ic_status_scanning;
+        Notification n = buildNotification(ctx, icon, title, text, contentIntent);
+        startForeground(NOTIFICATION_ID, n);
     }
 
     private void cancelNotification() {

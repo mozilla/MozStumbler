4e3257428aa536d3d94fd9b4316a59448f7dd2ec
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2014-04-10 18:20:24 -0300

    Make StumblerBundle Parcelable.

--- src/org/mozilla/mozstumbler/Reporter.java
+++ src/org/mozilla/mozstumbler/Reporter.java
@@ -149,6 +149,12 @@ final class Reporter extends BroadcastReceiver {
             return;
         }
 
+        /*
+        Intent broadcast = new Intent(StumblerService.MESSAGE_TOPIC);
+        broadcast.putExtra("stumblerBundle", mBundle);
+        mContext.sendBroadcast(broadcast);
+        */
+
         Location position = mBundle.getGpsPosition();
         Collection<CellInfo> cells = mBundle.getCellData().values();
         Collection<ScanResult> wifis = mBundle.getWifiData().values();
--- src/org/mozilla/mozstumbler/StumblerBundle.java
+++ src/org/mozilla/mozstumbler/StumblerBundle.java
@@ -2,19 +2,82 @@ package org.mozilla.mozstumbler;
 
 import android.location.Location;
 import android.net.wifi.ScanResult;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.os.Parcel;
 
 import org.mozilla.mozstumbler.cellscanner.CellInfo;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-final class StumblerBundle {
+final class StumblerBundle implements Parcelable {
     private final Location mGpsPosition;
-    private final Map<String, ScanResult> mWifiData = new HashMap<String, ScanResult>();
-    private final Map<String, CellInfo> mCellData = new HashMap<String, CellInfo>();
+    private final Map<String, ScanResult> mWifiData;
+    private final Map<String, CellInfo> mCellData;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        Bundle wifiBundle = new Bundle(ScanResult.class.getClassLoader());
+        Collection<String> scans = mWifiData.keySet();
+        for (String s : scans) {
+            wifiBundle.putParcelable(s, mWifiData.get(s));
+        }
+
+        Bundle cellBundle = new Bundle(CellInfo.class.getClassLoader());
+        Collection<String> cells = mCellData.keySet();
+        for (String c : cells) {
+            cellBundle.putParcelable(c, mCellData.get(c));
+        }
+
+        out.writeBundle(wifiBundle);
+        out.writeBundle(cellBundle);
+        out.writeParcelable(mGpsPosition, 0);
+    }
+
+    public static final Parcelable.Creator<StumblerBundle> CREATOR
+        = new Parcelable.Creator<StumblerBundle>() {
+        @Override
+        public StumblerBundle createFromParcel(Parcel in) {
+            return new StumblerBundle(in);
+        }
+
+        @Override
+        public StumblerBundle[] newArray(int size) {
+            return new StumblerBundle[size];
+        }
+    };
+
+    private StumblerBundle(Parcel in) {
+        mWifiData = new HashMap<String, ScanResult>();
+        mCellData = new HashMap<String, CellInfo>();
+
+        Bundle wifiBundle = in.readBundle(ScanResult.class.getClassLoader());
+        Bundle cellBundle = in.readBundle(CellInfo.class.getClassLoader());
+
+        Collection<String> scans = wifiBundle.keySet();
+        for (String s : scans) {
+            mWifiData.put(s, (ScanResult) wifiBundle.get(s));
+        }
+
+        Collection<String> cells = cellBundle.keySet();
+        for (String c : cells) {
+            mCellData.put(c, (CellInfo) cellBundle.get(c));
+        }
+
+        mGpsPosition = in.readParcelable(Location.class.getClassLoader());
+    }
 
     public StumblerBundle(Location position) {
         mGpsPosition = position;
+        mWifiData = new HashMap<String, ScanResult>();
+        mCellData = new HashMap<String, CellInfo>();
     }
 
     public Location getGpsPosition() {

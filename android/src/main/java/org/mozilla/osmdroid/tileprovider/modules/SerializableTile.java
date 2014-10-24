7e77d5e2fa2f81f14d6b379eb4fe35b3dc413597
Victor Ng <victor@crankycoder.com>
2014-10-24 17:54:52 -0400

    init checkin of a tile file which also contains header data

---
+++ android/src/main/java/org/mozilla/osmdroid/tileprovider/modules/SerializableTile.java
@@ -0,0 +1,177 @@
+package org.mozilla.osmdroid.tileprovider.modules;
+
+import org.apache.http.util.ByteArrayBuffer;
+import org.mozilla.mozstumbler.service.AppGlobals;
+import org.mozilla.mozstumbler.service.core.logging.Log;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by victorng on 14-10-24.
+ *
+ * This class represents a serializable Tile.
+ */
+public class SerializableTile {
+
+    private static final String LOG_TAG =
+            AppGlobals.LOG_PREFIX + SerializableTile.class.getSimpleName();
+
+    final byte[] FILE_HEADER = {(byte) 0xde, (byte) 0xca, (byte) 0xfb, (byte) 0xad};
+
+    byte[] tData;
+    Map<String, String> headers;
+
+    public SerializableTile() {
+        headers = new HashMap<String, String>();
+    }
+
+
+    public void setTileData(byte[] tileData) {
+        tData = tileData;
+    }
+
+    public byte[] getTileData() {
+        return tData;
+    }
+
+    public void setHeaders(Map<String, String> h) {
+        headers = new HashMap<String, String>();
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            if (entry.getValue() == null || entry.getKey() == null) {
+                // skip over this
+                continue;
+            }
+            headers.put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public void clearHeaders() {
+        headers.clear();
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    /*
+     Write out the tile data as:
+
+     Header:
+
+     4 byte int header to signify this is a special file
+
+        0xde 0xca 0xfb 0xad
+
+     4 byte int declaring the number of headers
+
+         Each header is written as:
+         4 byte int declaring length of header name in UTF-8 bytes
+         4 byte int declaring length of header value in raw bytes
+         X bytes for the header
+         Y bytes for the value
+
+     4 bytes declaring content body length
+     M bytes for content body
+
+     */
+    public byte[] asBytes() throws CharacterCodingException {
+
+        Charset charsetE = Charset.forName("UTF-8");
+        CharsetEncoder encoder = charsetE.newEncoder();
+
+
+        ByteArrayBuffer buff = new ByteArrayBuffer(10);
+        buff.append(FILE_HEADER, 0, FILE_HEADER.length);
+
+        buff.append(headers.size());
+
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            byte[] keyBytes = encoder.encode(CharBuffer.wrap(entry.getKey())).array();
+            byte[] valueBytes = encoder.encode(CharBuffer.wrap(entry.getValue())).array();
+
+            buff.append(keyBytes.length);
+            buff.append(keyBytes, 0, keyBytes.length);
+            buff.append(valueBytes, 0, valueBytes.length);
+        }
+
+        buff.append(tData.length);
+        buff.append(tData, 0, tData.length);
+
+        return buff.toByteArray();
+    }
+
+    public boolean fromFile(File file) throws CharacterCodingException, FileNotFoundException {
+        Charset charsetE = Charset.forName("UTF-8");
+        CharsetDecoder decoder = charsetE.newDecoder();
+
+        FileInputStream fis = new FileInputStream(file);
+        byte[] arr = new byte[(int) file.length()];
+        byte[] buffer = new byte[(int) file.length()];
+
+        try {
+            fis.read(arr);
+        } catch (IOException e) {
+            Log.w(LOG_TAG, "Error reading file into array.");
+        }
+
+        //  create a byte buffer and wrap the array
+        ByteBuffer bb = ByteBuffer.wrap(arr);
+
+
+        //  read your integers using ByteBuffer's getInt().
+        //  four bytes converted into an integer!
+        bb.get(buffer, 0, 4);
+        if (!Arrays.equals(buffer, FILE_HEADER)) {
+            // invalid header
+            return false;
+        }
+
+        // read # of headers
+        int headerCount = 0;
+        headerCount = bb.getInt();
+        headers.clear();
+
+        for (int i=0; i < headerCount; i++) {
+            int keyLength = bb.getInt();
+            int valueLength = bb.getInt();
+
+            buffer = new byte[keyLength];
+            String key = decoder.decode(bb.get(buffer, 0, keyLength)).toString();
+
+            buffer = new byte[valueLength];
+            String value = decoder.decode(bb.get(buffer, 0, valueLength)).toString();
+            headers.put(key, value);
+        }
+
+        // Remaining bytes should equal the content length of our payload.
+        int contentLength = bb.getInt();
+        if (bb.remaining() != contentLength) {
+            return false;
+        }
+
+        tData = new byte[contentLength];
+        bb.get(tData, 0, contentLength);
+
+        return true;
+    }
+
+
+
+
+}

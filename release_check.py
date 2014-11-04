37b7a14798acdde1adb10adf630bc8404027d28e
Victor Ng <victor@crankycoder.com>
2014-11-04 18:36:22 -0500

    patch the release_check.py as we don't keep ACRA information in the private properties anymore

--- release_check.py
+++ release_check.py
@@ -12,6 +12,3 @@ for line in open('android/properties/private-%s.properties' % sys.argv[-1]):
     props[k] = v
 assert 'MapAPIKey' in props, 'MapAPIKey is ok'
 assert 'MozAPIKey' in props, 'MozAPIKey is ok'
-assert 'ACRA_URI' in props, 'ACRA_URI is ok'
-assert 'ACRA_USER' in props, 'ACRA_USER is ok'
-assert 'ACRA_PASS' in props, 'ACRA_PASS is ok'

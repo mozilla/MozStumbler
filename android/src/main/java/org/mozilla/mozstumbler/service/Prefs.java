1eb205af50f889269d28b011c3f9a45455cbe313
Victor Ng <victor@crankycoder.com>
2014-11-20 14:13:33 -0500

    optimized imports and reformatted Prefs.java

--- android/src/main/java/org/mozilla/mozstumbler/service/Prefs.java
+++ android/src/main/java/org/mozilla/mozstumbler/service/Prefs.java
@@ -13,10 +13,11 @@ import android.text.TextUtils;
 import android.util.Log;
 
 public class Prefs {
-    private static final String LOG_TAG = AppGlobals.makeLogTag(Prefs.class.getSimpleName());
     public static final String NICKNAME_PREF = "nickname";
     public static final String EMAIL_PREF = "email";
     public static final String WIFI_ONLY = "wifi_only";
+    protected static final String PREFS_FILE = Prefs.class.getSimpleName();
+    private static final String LOG_TAG = AppGlobals.makeLogTag(Prefs.class.getSimpleName());
     private static final String USER_AGENT_PREF = "user-agent";
     private static final String VALUES_VERSION_PREF = "values_version";
     private static final String LAT_PREF = "lat_pref";
@@ -24,16 +25,12 @@ public class Prefs {
     private static final String FIREFOX_SCAN_ENABLED = "firefox_scan_on";
     private static final String MOZ_API_KEY = "moz_api_key";
     private static final String LAST_ATTEMPTED_UPLOAD_TIME = "last_attempted_upload_time";
-    protected static final String PREFS_FILE = Prefs.class.getSimpleName();
-
     // Simulation prefs
     private static final String SIMULATE_STUMBLE = "simulate_stumble";
     private static final String SIMULATION_LAT = "simulate_lat";
     private static final String SIMULATION_LON = "simulate_lon";
-
-
-    private final SharedPreferences mSharedPrefs;
     protected static Prefs sInstance;
+    private final SharedPreferences mSharedPrefs;
 
     protected Prefs(Context context) {
         mSharedPrefs = context.getSharedPreferences(PREFS_FILE, Context.MODE_MULTI_PROCESS | Context.MODE_PRIVATE);
@@ -60,8 +57,25 @@ public class Prefs {
 
     /* Only access after CreatePrefsInstance(Context) has been called at startup. */
     public static synchronized Prefs getInstance() {
-        assert(sInstance != null);
+        assert (sInstance != null);
         return sInstance;
+    }
+
+    @TargetApi(9)
+    protected static void apply(SharedPreferences.Editor editor) {
+        if (VERSION.SDK_INT >= 9) {
+            editor.apply();
+        } else if (!editor.commit()) {
+            Log.e(LOG_TAG, "", new IllegalStateException("commit() failed?!"));
+        }
+    }
+
+    ///
+    /// Getters
+    ///
+    public synchronized String getUserAgent() {
+        String s = getStringPref(USER_AGENT_PREF);
+        return (s == null) ? AppGlobals.appName + "/" + AppGlobals.appVersionName : s;
     }
 
     ///
@@ -71,35 +85,33 @@ public class Prefs {
         setStringPref(USER_AGENT_PREF, userAgent);
     }
 
-    public synchronized void setUseWifiOnly(boolean state) {
-        setBoolPref(WIFI_ONLY, state);
+    public synchronized boolean getFirefoxScanEnabled() {
+        return getBoolPrefWithDefault(FIREFOX_SCAN_ENABLED, false);
+    }
+
+    public synchronized void setFirefoxScanEnabled(boolean on) {
+        setBoolPref(FIREFOX_SCAN_ENABLED, on);
+    }
+
+    public synchronized String getMozApiKey() {
+        String s = getStringPref(MOZ_API_KEY);
+        return (s == null) ? "no-mozilla-api-key" : s;
     }
 
     public synchronized void setMozApiKey(String s) {
         setStringPref(MOZ_API_KEY, s);
     }
 
-    ///
-    /// Getters
-    ///
-    public synchronized String getUserAgent() {
-        String s = getStringPref(USER_AGENT_PREF);
-        return (s == null)? AppGlobals.appName + "/" + AppGlobals.appVersionName : s;
-    }
-
-    public synchronized boolean getFirefoxScanEnabled() {
-        return getBoolPrefWithDefault(FIREFOX_SCAN_ENABLED, false);
-    }
-
-    public synchronized String getMozApiKey() {
-        String s = getStringPref(MOZ_API_KEY);
-        return (s == null)? "no-mozilla-api-key" : s;
-    }
-
     // This is the time an upload was last attempted, not necessarily successful.
     // Used to ensure upload attempts aren't happening too frequently.
     public synchronized long getLastAttemptedUploadTime() {
         return getPrefs().getLong(LAST_ATTEMPTED_UPLOAD_TIME, 0);
+    }
+
+    public synchronized void setLastAttemptedUploadTime(long time) {
+        SharedPreferences.Editor editor = getPrefs().edit();
+        editor.putLong(LAST_ATTEMPTED_UPLOAD_TIME, time);
+        apply(editor);
     }
 
     public synchronized String getNickname() {
@@ -110,6 +122,12 @@ public class Prefs {
         return TextUtils.isEmpty(nickname) ? null : nickname;
     }
 
+    public synchronized void setNickname(String nick) {
+        if (nick != null) {
+            nick = nick.trim();
+            setStringPref(NICKNAME_PREF, nick);
+        }
+    }
 
     public synchronized String getEmail() {
         String email = getStringPref(EMAIL_PREF);
@@ -119,27 +137,10 @@ public class Prefs {
         return TextUtils.isEmpty(email) ? null : email;
     }
 
-    public synchronized void setFirefoxScanEnabled(boolean on) {
-        setBoolPref(FIREFOX_SCAN_ENABLED, on);
-    }
-
-    public synchronized void setLastAttemptedUploadTime(long time) {
-        SharedPreferences.Editor editor = getPrefs().edit();
-        editor.putLong(LAST_ATTEMPTED_UPLOAD_TIME, time);
-        apply(editor);
-    }
-
     public synchronized void setEmail(String email) {
         if (email != null) {
             email = email.trim();
             setStringPref(EMAIL_PREF, email);
-        }
-    }
-
-    public synchronized void setNickname(String nick) {
-        if (nick != null) {
-            nick = nick.trim();
-            setStringPref(NICKNAME_PREF, nick);
         }
     }
 
@@ -151,6 +152,10 @@ public class Prefs {
     /// Privates
     ///
 
+    public synchronized void setUseWifiOnly(boolean state) {
+        setBoolPref(WIFI_ONLY, state);
+    }
+
     protected String getStringPref(String key) {
         return getPrefs().getString(key, null);
     }
@@ -161,7 +166,7 @@ public class Prefs {
 
     protected void setBoolPref(String key, Boolean state) {
         SharedPreferences.Editor editor = getPrefs().edit();
-        editor.putBoolean(key,state);
+        editor.putBoolean(key, state);
         apply(editor);
     }
 
@@ -169,27 +174,16 @@ public class Prefs {
         return getPrefs().getFloat(key, def);
     }
 
-
     protected void setFloatPref(String key, float value) {
         SharedPreferences.Editor editor = getPrefs().edit();
         editor.putFloat(key, value);
         apply(editor);
     }
 
-
     protected void setStringPref(String key, String value) {
         SharedPreferences.Editor editor = getPrefs().edit();
         editor.putString(key, value);
         apply(editor);
-    }
-
-    @TargetApi(9)
-    protected static void apply(SharedPreferences.Editor editor) {
-        if (VERSION.SDK_INT >= 9) {
-            editor.apply();
-        } else if (!editor.commit()) {
-            Log.e(LOG_TAG, "", new IllegalStateException("commit() failed?!"));
-        }
     }
 
     @SuppressLint("InlinedApi")
@@ -209,20 +203,20 @@ public class Prefs {
         setBoolPref(SIMULATE_STUMBLE, b);
     }
 
-    public void setSimulationLat(float value) {
-        setFloatPref(SIMULATION_LAT, value);
-    }
-
-    public void setSimulationLon(float value) {
-        setFloatPref(SIMULATION_LON, value);
-    }
-
     public float getSimulationLat() {
         return getFloatPref(SIMULATION_LAT, (float) 43.6472969);
     }
 
+    public void setSimulationLat(float value) {
+        setFloatPref(SIMULATION_LAT, value);
+    }
+
     public float getSimulationLon() {
-        return getFloatPref(SIMULATION_LON, (float)-79.3943137);
+        return getFloatPref(SIMULATION_LON, (float) -79.3943137);
+    }
+
+    public void setSimulationLon(float value) {
+        setFloatPref(SIMULATION_LON, value);
     }
 
 }

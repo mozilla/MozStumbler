68988812682fd3a4354048444b4b2ecafc6b38bf
Victor Ng <victor@crankycoder.com>
2015-04-06 15:46:55 -0400

    Reworked exception handling to remove all IOExceptions from the main codepath when reading or writing Reports from storage. This should give us much better errors about what is actually happening and more graceful error handling.

--- android/src/test/java/org/mozilla/mozstumbler/service/stumblerthread/datahandling/DataStorageManagerTest.java
+++ android/src/test/java/org/mozilla/mozstumbler/service/stumblerthread/datahandling/DataStorageManagerTest.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.location.Location;
 import android.net.wifi.ScanResult;
+import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
 
 import org.json.JSONException;
@@ -11,6 +12,7 @@ import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mozilla.mozstumbler.service.Prefs;
 import org.mozilla.mozstumbler.service.stumblerthread.Reporter;
 import org.mozilla.mozstumbler.service.stumblerthread.scanners.cellscanner.CellInfo;
 import org.robolectric.Robolectric;
@@ -41,11 +43,16 @@ public class DataStorageManagerTest {
     public void setUp() {
         ctx = getApplicationContext();
 
+        // Disable stumble logs
+        Prefs.getInstance(ctx).setSaveStumbleLogs(false);
         StorageTracker tracker = new StorageTracker();
 
         long maxBytes = 20000;
         int maxWeeks = 10;
 
+        // We need to explicitly clear the global instance or else we can't guarantee that the
+        // directory will be properly created
+        ClientDataStorageManager.sInstance = null;
         dm = ClientDataStorageManager.createGlobalInstance(ctx, tracker, maxBytes, maxWeeks);
         // Force the current reports to clear out between test runs.
         dm.mCurrentReports.clearReports();
@@ -62,6 +69,7 @@ public class DataStorageManagerTest {
         StumblerBundle bundle;
 
         assertEquals(0, dm.mCurrentReports.reportsCount());
+
         for (int locCount = 0; locCount < ReportBatchBuilder.MAX_REPORTS_IN_MEMORY - 1; locCount++) {
             Location loc = new Location("mock");
             loc.setLatitude(42 + (locCount * 0.1));
@@ -84,18 +92,14 @@ public class DataStorageManagerTest {
             JSONObject mlsObj = bundle.toMLSGeosubmit();
             int wifiCount = mlsObj.getJSONArray(DataStorageContract.ReportsColumns.WIFI).length();
             int cellCount = mlsObj.getJSONArray(DataStorageContract.ReportsColumns.CELL).length();
-            try {
-                dm.insert(mlsObj.toString(), wifiCount, cellCount);
-            } catch (IOException ioEx) {
-            }
+            dm.insert(mlsObj.toString(), wifiCount, cellCount);
+
+            assertEquals((locCount+1) % ReportBatchBuilder.MAX_REPORTS_IN_MEMORY,
+                    dm.mCurrentReports.reportsCount());
         }
 
-        assertEquals(ReportBatchBuilder.MAX_REPORTS_IN_MEMORY - 1,
-                dm.mCurrentReports.reportsCount());
-
-
-        for (int locCount = ReportBatchBuilder.MAX_REPORTS_IN_MEMORY;
-             locCount < ReportBatchBuilder.MAX_REPORTS_IN_MEMORY + 100;
+        for (int locCount = ReportBatchBuilder.MAX_REPORTS_IN_MEMORY -1;
+             locCount < (ReportBatchBuilder.MAX_REPORTS_IN_MEMORY*2-1);
              locCount++) {
             Location loc = new Location("mock");
             loc.setLatitude(42 + (locCount * 0.1));
@@ -118,15 +122,14 @@ public class DataStorageManagerTest {
             JSONObject mlsObj = bundle.toMLSGeosubmit();
             int wifiCount = mlsObj.getJSONArray(DataStorageContract.ReportsColumns.WIFI).length();
             int cellCount = mlsObj.getJSONArray(DataStorageContract.ReportsColumns.CELL).length();
-            try {
-                dm.insert(mlsObj.toString(), wifiCount, cellCount);
-            } catch (FileNotFoundException fex) {
-                // This is ok
-            } catch (IOException ioEx) {
-            }
 
-            assertTrue(dm.mCurrentReports.reportsCount() == ReportBatchBuilder.MAX_REPORTS_IN_MEMORY);
+            dm.insert(mlsObj.toString(), wifiCount, cellCount);
+
+            assertEquals((locCount+1) % ReportBatchBuilder.MAX_REPORTS_IN_MEMORY  ,
+                    dm.mCurrentReports.reportsCount());
+
         }
+
     }
 
     public class StorageTracker implements DataStorageManager.StorageIsEmptyTracker {

ee62711c28d95e4cd0247b06663f3c0eb44cdb20
Victor Ng <victor@crankycoder.com>
2015-04-07 23:50:01 -0400

    added docs to IDataStorageManager to clean up the interface

--- libraries/stumbler/src/main/java/org/mozilla/mozstumbler/service/stumblerthread/datahandling/IDataStorageManager.java
+++ libraries/stumbler/src/main/java/org/mozilla/mozstumbler/service/stumblerthread/datahandling/IDataStorageManager.java
@@ -11,36 +11,55 @@ import java.io.IOException;
  */
 public interface IDataStorageManager {
 
+    // Some stats retrieval methods on the DSM
     public int getQueuedReportCount();
-
     public int getQueuedWifiCount();
-
     public int getQueuedCellCount();
-
     public long getQueuedZippedDataSize();
-
     public byte[] getCurrentReportsRawBytes();
 
-    public boolean delete(String filename);
-
-    public void saveCurrentReportsSendBufferToDisk() throws IOException;
-
-    public ReportBatch getFirstBatch() throws IOException;
-
-    public ReportBatch getNextBatch() throws IOException;
-
-    public void incrementSyncStats(long bytesSent, long reports, long cells, long wifis) throws IOException;
-
+    // Insert a report into storage
     public void insert(String report, int wifiCount, int cellCount) throws IOException;
 
-    public void saveCurrentReportsToDisk() throws IOException;
+    // It feels like this method should be pushed behind the DataStorageManager implementation
+    public void incrementSyncStats(long bytesSent, long reports, long cells, long wifis) throws IOException;
 
+
+    // This is used only by the StumblerService on initial startup of the service in the
+    // `onHandleIntent` method.  We use it to test if there are any files waiting to be uploaded
+    // on initial process startup.
+    // We should get rid of this method `isDirEmpty` and instead have a method like
+    // `uploadDataOnProcessStartup` and push the logic from StumblerService down into the
+    // DataStorageManager.
     public boolean isDirEmpty();
 
+
+    // It seems like we should be able to get rid of the sendBuffer which is just a ReportBatch
+    // record. This would get rid of having to maintain the state of `mCurrentReportsSendBuffer`
+    // and get rid of the saveCurrentReportsSendBufferToDisk method.
+    public void saveCurrentReportsSendBufferToDisk() throws IOException;
+    public void saveCurrentReportsToDisk() throws IOException;
+
+
+    // Getting an instance of ReportBatch should really hang off of an iterator.
+    // We have two methods because we have a special case where the first batch is
+    // all reports that are currently stored only in-memory.  getNextBatch will always hit disk.
+    // We should extend the ReportBatchIterator to have knowledge of the in-memory reports
+    // and implement java.util.Iterator to return the ReportBatch instances.
+    public ReportBatch getFirstBatch() throws IOException;
+    public ReportBatch getNextBatch() throws IOException;
+
+    // These 3 methods are *only* used to determine if we have stale data and if we should be
+    // clearing it out.  We should collapse these into one method call and return a boolean
+    // to indicate that the pending reports have been truncated.
     public long getOldestBatchTimeMs();
-
     public int getMaxWeeksStored();
-
     public void deleteAll();
+
+
+
+    // delete should be pushed down into ReportBatch as it already has everything except
+    // DataStorageManager::mReportsDir to do this work.
+    public boolean delete(String filename);
 
 }
